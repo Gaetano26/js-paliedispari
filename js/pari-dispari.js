b808493f8cd72ae43e55b5d51e5346369bfe5ff1
Gaetano26 <gaetano.spampinato@outlook.it>
2023-03-16 17:58:49 +0100

    start pari e dispari

---
+++ js/pari-dispari.js
@@ -0,0 +1,20 @@
+/*Pari e Dispari
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.
+*/
+
+let select = document.querySelector ('select');
+let num = parseInt(document.getElementById("number"));
+let pari = parseInt(document.getElementById("pari"));
+let dispari = parseInt(document.getElementById("dispari"));
+const btn= document.querySelector('button');
+btn.addEventListener('click', play);
+
+function play () {
+
+}
+
+
