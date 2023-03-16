ef4f10482405dfb761b1382a6ba33df91ba46b71
Gaetano26 <gaetano.spampinato@outlook.it>
2023-03-16 19:31:08 +0100

    fixed

--- js/pari-dispari.js
+++ js/pari-dispari.js
@@ -5,16 +5,58 @@ Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.
 */
-
-let select = document.querySelector ('select');
+let number = document.getElementById ('number')
+const select = document.querySelector('select');
 const btn= document.querySelector('button');
 btn.addEventListener('click', play);
 
 function play () {
-     let sceltaGiocatore =  select.value;
+    let scelta = document.querySelector('select').value;
      let numeroGiocatore = document.getElementById('number').value;
      let numeroComputer = Math.floor((Math.random() * 5) + 1);
-     console.log(numeroGiocatore)
+     //console.log(numeroGiocatore)
+    let somma = numeroComputer + parseInt(numeroGiocatore);
+    let risultato = somma % 2;
+
+    if (somma === 0) {
+        somma = 'pari'
+    } else {
+        somma = 'dispari'
+    }
 }
 
+if (risultato === scelta) {
+    document.getElementById ('risultato').innerHTML =`
+    <div>
+       <h4 class="text-danger">Tu</h4>
+       <p class="pt-4">${numeroGiocatore}</p>    
+    </div>
+    <div class="ps-5" >
+       <h4 class="text-danger">Computer</h4>
+       <p class="pt-4">${numeroComputer}</p>    
+    </div>
+    `;
+    document.getElementById ('vincitore').innerHTML =`
+    <div>
+       <h4 class="text-danger">Hai vinto!</h4>    
+    </div>
+    `;
+
+} else {
+    document.getElementById ('risultato').innerHTML =`
+    <div>
+       <h4 class="text-danger">Tu</h4>
+       <p class="pt-4">${numeroGiocatore}</p>    
+    </div>
+    <div class="ps-5" >
+       <h4 class="text-danger">Computer</h4>
+       <p class="pt-4">${numeroComputer}</p>    
+    </div>
+    `;
+    document.getElementById ('vincitore').innerHTML =`
+    <div>
+       <h4 class="text-danger">hai perso!</h4>    
+    </div>
+    `;
+}
 
