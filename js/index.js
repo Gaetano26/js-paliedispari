7c9aeea6412061010f1d8e3a0d5ebbf5899c26cc
Gaetano26 <gaetano.spampinato@outlook.it>
2023-03-16 16:30:54 +0100

    fixed

--- js/index.js
+++ js/index.js
@@ -13,27 +13,28 @@ const btn = document.getElementById('invia');
 
  //funzione per verificare se la parola  inserita è valida oppure no
  function entra () {
-    let cerca = document.getElementById('invia').value;
+    let cerca = document.getElementById('inserisci').value;
     let parola = cerca;
-    let carattere = '';
+    let carattere;
     let parolaInversa = '';
-    const i = parola.length - 1;
+    let i = parola.length -1; 
 
     while (i >= 0) {
         carattere = parola[i];
         parolaInversa += carattere;
         i--;
     }
-      
-    //se la funzione è vera genera il testo + il bottone con il link per entrare nel gioco
-    if (parola === parolaInversa ) {
+
+    console.log(parolaInversa)
+      //se la funzione è vera genera i genera il testo è l'utente non può piu andare avanti
+    if (parola == parolaInversa ) {
         document.getElementById("inserisci").style.display = "none";
         document.querySelector(".container").innerHTML = `
                <div class="d-flex flex-column justify-content-center align-items-center">
                    <h1 class="text-danger">Non sei stato invitato : lascia la pagina!</h1>
                </div> 
         `;
-      //se la funzione è falsa genera il testo è l'utente non può piu andare avanti
+      //se la funzione è falsa genera il testo  + il bottone per andare avanti
     } else {
         document.getElementById("inserisci").style.display = "none";
         document.querySelector(".container").innerHTML = `
