5445b61264c59febe542f5d81ca59ea993931d25
Gaetano26 <gaetano.spampinato@outlook.it>
2023-03-16 16:41:26 +0100

    palindrome complete

--- js/index.js
+++ js/index.js
@@ -13,7 +13,7 @@ const btn = document.getElementById('invia');
 
  //funzione per verificare se la parola  inserita è valida oppure no
  function entra () {
-    let cerca = document.getElementById('inserisci').value;
+    let cerca = document.getElementById('ins').value;
     let parola = cerca;
     let carattere;
     let parolaInversa = '';
@@ -26,12 +26,12 @@ const btn = document.getElementById('invia');
     }
 
     console.log(parolaInversa)
-      //se la funzione è vera genera i genera il testo è l'utente non può piu andare avanti
+      //se la funzione è vera genera il testo è l'utente non può piu andare avanti
     if (parola == parolaInversa ) {
         document.getElementById("inserisci").style.display = "none";
         document.querySelector(".container").innerHTML = `
                <div class="d-flex flex-column justify-content-center align-items-center">
-                   <h1 class="text-danger">Non sei stato invitato : lascia la pagina!</h1>
+                   <h1 class="text-danger">Parola palindroma : ricarica la pagina!</h1>
                </div> 
         `;
       //se la funzione è falsa genera il testo  + il bottone per andare avanti
@@ -40,7 +40,7 @@ const btn = document.getElementById('invia');
         document.querySelector(".container").innerHTML = `
         <div class="d-flex flex-column justify-content-center align-items-center">
         <h1> Benvenuto! </h1>
-       <button type="button" class="btn btn-success"><a class="text-white text-decoration-none fs-4" href="./dadi.html">Gioca</a></button>
+       <button type="button" class="btn btn-success"><a class="text-white text-decoration-none fs-4" href="./pari-dispari.html">Gioca</a></button>
       </div> 
     `;
     }
